ea1f6891cd5e68b54fcaa7c7cb28fe1d560c8dfb
kanalavinodkumar <kanalavinodkumar@gmail.com>
2024-02-15 18:09:42 +0530

    provider

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -2,20 +2,19 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.15.0"
+      version = "5.30.0"
     }
   }
 
   backend "s3" {
-    bucket   = "vinod-roboshop"
-    key = "dev"
-    region = "us-east-1"
-    dynamodb_table = "vinod-roboshop"
+        bucket = "vinod-tf-workspace"
+        key    = "state"
+        region = "us-east-1"
+        dynamodb_table = "vinod-tf-workspace"
   }
+  
 }
 
 provider "aws" {
   # Configuration options
-  # you can give access key and secret key here, but security problem
-  region = "us-east-1"
 }
